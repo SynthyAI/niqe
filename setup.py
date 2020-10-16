9a78eb475c61f3bad2eed52c034c8d4edb81c00b
Dino Vougioukas <1238366+DinoMan@users.noreply.github.com>
2020-10-16 15:22:01 +0100

    added travis file

--- setup.py
+++ setup.py
@@ -19,5 +19,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-video',
+        'opencv-python',
+        ]
     python_requires='>=3.6',
 )
